674ec7b9bb205d61477459bb18449b04ddcf490c
StevenK418 <stevenkelly418@gmail.com>
2022-11-24 15:39:17 +0000

    => Added new query code to get and print all results with the name 'Joe'.

--- app/src/main/java/com/example/sqlite/MainActivity.java
+++ app/src/main/java/com/example/sqlite/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.sqlite;
 import android.os.Bundle;
 import java.util.List;
 import android.app.ListActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
@@ -28,6 +29,9 @@ public class MainActivity extends ListActivity
         ArrayAdapter<Comment> adapter = new ArrayAdapter<Comment>(this,
                 android.R.layout.simple_list_item_1, values);
         setListAdapter(adapter);
+
+        //Populate the contacts db
+        PopulateContacts();
     }
 
     // Will be called via the onClick attribute
@@ -67,4 +71,60 @@ public class MainActivity extends ListActivity
         datasource.close();
         super.onPause();
     }
+
+    public void PopulateContacts()
+    {
+        ContactsDatabaseHandler db = new ContactsDatabaseHandler(this);
+
+        //db.emptyContacts();     // empty table if required
+
+        // Inserting Contacts
+        Log.i("Insert: ", "Inserting ..");
+        db.addContact(new Contact("Joe", "0873456789"));
+        db.addContact(new Contact("Mary", "0863111122"));
+        db.addContact(new Contact("Jack", "0859999888"));
+        db.addContact(new Contact("Andrew", "083334444"));
+        db.addContact(new Contact("Harold", "0831112222"));
+        db.addContact(new Contact("Joe", "0835554444"));
+
+        // Reading all contacts
+        Log.i("Reading: ", "Reading all contacts..");
+        List<Contact> contacts = db.getAllContacts();
+
+        for (Contact cn : contacts) {
+            String log = "Id: " + cn.getID() + " ,Name: " + cn.getName() + " ,Phone: " +
+                    cn.getPhoneNumber();
+            // Writing Contacts to log
+            Log.i("Name: ", log);
+        }
+
+        Log.i("divider", "====================");
+
+        Contact singleUser = db.getContact(5);
+        Log.i("contact 5 is ", singleUser.getName());
+
+        Log.i("divider", "====================");
+
+        // Calling SQL statement
+        int userCount = db.getContactsCount();
+        Log.i("User count: ", String.valueOf(userCount));
+
+        //Get all of the Joes
+        GetAllWithNameJoe(db);
+    }
+
+    // Gets all the contacts whose name is Joe
+    public void GetAllWithNameJoe(ContactsDatabaseHandler db )
+    {
+        //Create a new list to store the result of teh query
+        List<Contact> joeContacts =  db.getAllContactsWithNameJoe();
+        //Iterate through the result and print
+        for (Contact cn : joeContacts) {
+            String log = "Id: " + cn.getID() + " ,Name: " + cn.getName() + " ,Phone: " +
+                    cn.getPhoneNumber();
+            // Writing Contacts to log
+            Log.i("Name: ", log);
+        }
+    }
+
 }
